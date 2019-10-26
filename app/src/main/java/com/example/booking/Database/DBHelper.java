ea91792ba4001d90024d2e757b6a41abb33bb667
Shubham-Phatale <shubhamp11298@gmail.com>
2019-10-27 00:28:56 +0530

    first commit

--- app/src/main/java/com/example/booking/Database/DBHelper.java
+++ app/src/main/java/com/example/booking/Database/DBHelper.java
@@ -6,6 +6,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DBHelper extends SQLiteOpenHelper {
     String create_table = "create table user(name,email,age Integer,gender,password text not null)";
     public DBHelper(Context context) {
@@ -22,13 +25,13 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("drop Table if Exists user");
     }
 
-    public boolean insert (String name,String email,String age,String gender,String password){
+    public boolean insert (String name,String email,String age,String number,String password){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put("name",name);
         cv.put("email",email);
         cv.put("age",age);
-        cv.put("gender",gender);
+        cv.put("number",number);
         cv.put("password",password);
         long ins = db.insert("user",null,cv);
         if(ins==-1)return false;
@@ -45,5 +48,20 @@ public class DBHelper extends SQLiteOpenHelper {
         Cursor cursor = db.rawQuery("Select * from user where email=? and password=?",new String[] {email,password} );
         if (cursor.getCount() > 0) return false;
         else return true;
+    }
+    public ArrayList getuserinfo(String email){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ArrayList s = new ArrayList();
+        Cursor cursor = db.rawQuery("select name,age,number from user where email=?",new String[] {email});
+        if(cursor.moveToFirst()){
+            do{
+                s.add(cursor.getString(0));
+                s.add(cursor.getString(2));
+                s.add(cursor.getString(3));
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return s;
     }
 }
--- app/src/main/java/com/example/booking/profileFragment.java
+++ app/src/main/java/com/example/booking/profileFragment.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,17 +18,21 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.booking.Database.DBHelper;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class profileFragment extends Fragment {
 
@@ -34,11 +41,55 @@ public class profileFragment extends Fragment {
     LocationCallback callback;
     LocationRequest request;
     private LocationManager locationManager;
+    TextView txtloc;
+    DBHelper db;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        View v = inflater.inflate(R.layout.fragment_profile,container,false);
+        db = new DBHelper(getContext());
+        txtloc = v.findViewById(R.id.userlocation) ;
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+        callback = new LocationCallback(){
+
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                location = locationResult.getLocations().get(0);
+                Log.i("SHUB","Locarion =>" + location.getLatitude() + "," + location.getLatitude());
+                double lat = location.getLatitude();
+                double longi = location.getLongitude();
+                txtloc.setText("Location:\n\n" + lat + "," +longi);
+
+                convertAdress(location.getLatitude(),location.getLongitude());
+                super.onLocationResult(locationResult);
+            }
+
+        };
+        LocationRequest request = new LocationRequest();
+        request.setFastestInterval(3000);
+        request.setInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        client.requestLocationUpdates(request,callback, Looper.myLooper());
+
+        return v ;
     }
+
+    private void convertAdress(double latitude, double longitude) {
+
+        Geocoder geocoder = new Geocoder(getActivity());
+        try {
+            List<Address> add = geocoder.getFromLocation(latitude,longitude,1);
+            Address ad = add.get(0);
+            String address = ad.getAddressLine(0);
+            txtloc.setText(address);
+        }catch (Exception e){
+
+        }
+    }
+
+
 }
 
